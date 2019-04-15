e4dca03fe63f024ed6c87698cf6e7e9d01f9ea55
jjfallete <jfagel@allete.com>
2019-04-15 10:43:35 -0500

    Update cb_isolate_system.py

--- functions/carbon_black/cb_isolate_system.py
+++ functions/carbon_black/cb_isolate_system.py
@@ -48,7 +48,7 @@ class FunctionComponent(ResilientComponent):
 
             log = logging.getLogger(__name__)
 
-            hostname = hostname.upper().replace('@MNPOWER.COM', '').replace('.MNPOWER.COM', '')[:15]  # CB limits hostname to 15 characters
+            hostname = hostname.upper()[:15]  # CB limits hostname to 15 characters
             sensor = cb.select(Sensor).where('hostname:' + hostname)  # Query CB for the hostname's sensor
 
             if len(sensor) <= 0:  # Host does not have CB agent, abort
