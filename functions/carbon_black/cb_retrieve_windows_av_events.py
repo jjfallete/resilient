a02bd72ac2c1b8f3ff66258488f889fcbd9899ef
jjfallete <jfagel@allete.com>
2019-05-13 16:14:17 -0500

    Added host locking, miscellaneous edits
    Added hostname-based lock system to prevent concurrent live response functions to a single host that would inevitability cause a timeout when the first concurrent function closed the session upon its completion. You must create a 'cb_host_locks' directory in '/home/integrations/.resilient' prior.

--- functions/carbon_black/cb_retrieve_windows_av_events.py
+++ functions/carbon_black/cb_retrieve_windows_av_events.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 # pragma pylint: disable=unused-argument, no-self-use
 
-# This function will retrieve the Microsoft Antimalware and/or Windows Defender Windows event logs from an endpoint in corresponding TXT files.
+# This function will retrieve the Microsoft Security Client and/or Windows Defender Windows event logs from an endpoint in corresponding TXT files.
 # File: cb_retrieve_windows_av_events.py
-# Date: 04/15/2019 - Modified: 04/15/2019
+# Date: 04/15/2019 - Modified: 05/13/2019
 # Author: Jared F
 
 """Function implementation"""
@@ -14,8 +14,9 @@
 
 import os
 import time
-import logging
+import shutil
 import tempfile
+import logging
 import datetime
 from resilient_circuits import ResilientComponent, function, handler, StatusMessage, FunctionResult, FunctionError
 from cbapi.response import CbEnterpriseResponseAPI, Sensor
@@ -26,6 +27,9 @@ import carbon_black.util.selftest as selftest
 cb = CbEnterpriseResponseAPI()  # CB Response API
 MAX_TIMEOUTS = 3  # The number of CB timeouts that must occur before the function aborts
 DAYS_UNTIL_TIMEOUT = 3  # The number of days that must pass before the function aborts
+
+MAX_UPLOAD_SIZE = 50*1000000  # Maximum number of bytes of files to upload as an attachment before reverting to a netshare drop, default = 50MB
+TRANSFER_RATE = 225000  # Bytes per second, the expected minimum file transfer rate via Carbon Black
 
 
 class FunctionComponent(ResilientComponent):
@@ -44,11 +48,11 @@ class FunctionComponent(ResilientComponent):
 
     @function("cb_retrieve_windows_av_events")
     def _cb_retrieve_windows_av_events_function(self, event, *args, **kwargs):
-        """Function: Retrieves all Windows AV events from Windows Defender and Microsoft Security Client (Microsoft Antimalware)."""
 
         results = {}
         results["was_successful"] = False
         results["hostname"] = None
+        lock_acquired = False
 
         try:
             # Get the function parameters:
@@ -86,13 +90,19 @@ class FunctionComponent(ResilientComponent):
                     # Check online status
                     if sensor.status != "Online":
                         yield StatusMessage('[WARNING] Hostname: ' + str(hostname) + ' is offline. Will attempt for ' + str(DAYS_UNTIL_TIMEOUT) + ' days...')
-                    while (sensor.status != "Online") and (days_later_timeout_length >= now):  # Continuously check if the sensor comes online for 3 days
+
+                    # Check lock status
+                    if os.path.exists('/home/integrations/.resilient/cb_host_locks/{}.lock'.format(hostname)):
+                        yield StatusMessage('[WARNING] A running action has a lock on  ' + str(hostname) + '. Will attempt for ' + str(DAYS_UNTIL_TIMEOUT) + ' days...')
+
+                    # Wait for offline and locked hosts for days_later_timeout_length
+                    while (sensor.status != "Online" or os.path.exists('/home/integrations/.resilient/cb_host_locks/{}.lock'.format(hostname))) and (days_later_timeout_length >= now):
                         time.sleep(3)  # Give the CPU a break, it works hard!
                         now = datetime.datetime.now()
                         sensor = (cb.select(Sensor).where('hostname:' + hostname))[0]  # Retrieve the latest sensor vitals
 
                     # Abort after DAYS_UNTIL_TIMEOUT
-                    if sensor.status != "Online":
+                    if sensor.status != "Online" or os.path.exists('/home/integrations/.resilient/cb_host_locks/{}.lock'.format(hostname)):
                         yield StatusMessage('[FATAL ERROR] Hostname: ' + str(hostname) + ' is still offline!')
                         yield FunctionResult(results)
                         return
@@ -115,6 +125,14 @@ class FunctionComponent(ResilientComponent):
                             log.info('[FAILURE] Fatal error caused exit!')
                         return
 
+                    # Acquire host lock
+                    try:
+                        f = os.fdopen(os.open('/home/integrations/.resilient/cb_host_locks/{}.lock'.format(hostname), os.O_CREAT | os.O_WRONLY | os.O_EXCL), 'w')
+                        f.close()
+                        lock_acquired = True
+                    except OSError:
+                        continue
+
                     # Establish a session to the host sensor
                     yield StatusMessage('[INFO] Establishing session to CB Sensor #' + str(sensor.id) + ' (' + sensor.hostname + ')')
                     session = cb.live_response.request_session(sensor.id)
@@ -125,14 +143,16 @@ class FunctionComponent(ResilientComponent):
                     except Exception: pass  # Existed already
 
                     session.create_process(r'''cmd.exe /c wevtutil qe "System" /rd:True /q:"*[System[Provider[@Name='Microsoft Antimalware']]]" /f:Text > C:\Windows\CarbonBlack\Reports\Antimalware_Events.txt''', True, None, None, 300, True)  # Execute the utility
-                    yield StatusMessage('[SUCCESS] Queried all Microsoft Antimalware Windows events on Sensor!')
+                    yield StatusMessage('[SUCCESS] Queried all Microsoft Antimalware events on Sensor!')
 
                     session.create_process(r'''cmd.exe /c wevtutil qe "Microsoft-Windows-Windows Defender/Operational" /rd:True /f:Text > C:\Windows\CarbonBlack\Reports\Defender_Events.txt''', True, None, None, 300, True)  # Execute the utility
-                    yield StatusMessage('[SUCCESS] Queried all Windows Defender Windows events on Sensor!')
+                    yield StatusMessage('[SUCCESS] Queried all Windows Defender events on Sensor!')
 
                     with tempfile.NamedTemporaryFile(delete=False) as temp_file:  # Create temporary temp_file for TXT file
                         try:
-                            temp_file.write(session.get_file(r'C:\Windows\CarbonBlack\Reports\Antimalware_Events.txt'))  # Write the HTML file from the endpoint to temp_file
+                            file_size = session.list_directory(r'C:\Windows\CarbonBlack\Reports\Antimalware_Events.txt')[0]['size']  # File size in bytes
+                            custom_timeout = int((file_size / TRANSFER_RATE) + 120)  # The expected timeout duration + 120 seconds for good measure
+                            temp_file.write(session.get_file(r'C:\Windows\CarbonBlack\Reports\Antimalware_Events.txt', timeout=custom_timeout))  # Write the HTML file from the endpoint to temp_file
                             temp_file.close()
                             yield StatusMessage('[SUCCESS] Retrieved Microsoft Antimalware events data file from Sensor!')
                             if os.stat(temp_file.name).st_size == 0: yield StatusMessage('[SUCCESS] Microsoft Antimalware events data file is empty. Skipping...')  # If file is empty, don't send
@@ -145,13 +165,20 @@ class FunctionComponent(ResilientComponent):
 
                     with tempfile.NamedTemporaryFile(delete=False) as temp_file:  # Create temporary temp_file for TXT file
                         try:
-                            temp_file.write(session.get_file(r'C:\Windows\CarbonBlack\Reports\Defender_Events.txt'))  # Write the HTML file from the endpoint to temp_file
+                            file_size = session.list_directory(r'C:\Windows\CarbonBlack\Reports\Defender_Events.txt')[0]['size']  # File size in bytes
+                            custom_timeout = int((file_size / TRANSFER_RATE) + 120)  # The expected timeout duration + 120 seconds for good measure
+                            temp_file.write(session.get_file(r'C:\Windows\CarbonBlack\Reports\Defender_Events.txt', timeout=custom_timeout))  # Write the HTML file from the endpoint to temp_file
                             temp_file.close()
                             yield StatusMessage('[SUCCESS] Retrieved Windows Defender events data file from Sensor!')
+
                             if os.stat(temp_file.name).st_size == 0: yield StatusMessage('[SUCCESS] Windows Defender events data file is empty. Skipping...')  # If file is empty, don't send
-                            else:
+                            elif os.stat(temp_file.name).st_size <= MAX_UPLOAD_SIZE:
                                 self.rest_client().post_attachment('/incidents/{0}/attachments'.format(incident_id), temp_file.name, '{0}-Defender_Events.txt'.format(sensor.hostname))  # Post temp_file to incident
                                 yield StatusMessage('[SUCCESS] Posted Windows Defender events data file to the incident as an attachment!')
+                            else:
+                                if not os.path.exists(os.path.normpath('/mnt/cyber-sec-forensics/Resilient/{0}'.format(incident_id))): os.makedirs('/mnt/cyber-sec-forensics/Resilient/{0}'.format(incident_id))
+                                shutil.copyfile(temp_file.name, '/mnt/cyber-sec-forensics/Resilient/{0}/{1}-Defender_Events-{2}.txt'.format(incident_id, sensor.hostname, str(int(time.time()))))  # Post temp_file to network share
+                                yield StatusMessage('[SUCCESS] Posted Windows Defender events data file to the forensics network share!')
 
                         finally:
                             os.unlink(temp_file.name)  # Delete temporary temp_file
@@ -162,7 +189,7 @@ class FunctionComponent(ResilientComponent):
                 except TimeoutError:  # Catch TimeoutError and handle
                     timeouts = timeouts + 1
                     if timeouts <= MAX_TIMEOUTS:
-                        yield StatusMessage('[ERROR] TimeoutError was encountered. Reattempting... (' + str(timeouts) + '/3)')
+                        yield StatusMessage('[ERROR] TimeoutError was encountered. Reattempting... (' + str(timeouts) + '/' + str(MAX_TIMEOUTS) + ')')
                         try: session.close()
                         except: pass
                         sensor = (cb.select(Sensor).where('hostname:' + hostname))[0]  # Retrieve the latest sensor vitals
@@ -178,7 +205,7 @@ class FunctionComponent(ResilientComponent):
                     if 'ApiError' in str(type(err).__name__) and 'network connection error' not in str(err): raise  # Only handle ApiError involving network connection error
                     timeouts = timeouts + 1
                     if timeouts <= MAX_TIMEOUTS:
-                        yield StatusMessage('[ERROR] Carbon Black was unreachable. Reattempting in 30 minutes... (' + str(timeouts) + '/3)')
+                        yield StatusMessage('[ERROR] Carbon Black was unreachable. Reattempting in 30 minutes... (' + str(timeouts) + '/' + str(MAX_TIMEOUTS) + ')')
                         time.sleep(1800)  # Sleep for 30 minutes, backup service may have been running.
                     else:
                         yield StatusMessage('[FATAL ERROR] ' + str(type(err).__name__) + ' was encountered. The maximum number of retries was reached. Aborting!')
@@ -196,6 +223,10 @@ class FunctionComponent(ResilientComponent):
                 except: pass
                 yield StatusMessage('[INFO] Session has been closed to CB Sensor #' + str(sensor.id) + '(' + sensor.hostname + ')')
                 break
+
+            # Release the host lock if acquired
+            if lock_acquired is True:
+                os.remove('/home/integrations/.resilient/cb_host_locks/{}.lock'.format(hostname))
 
             # Produce a FunctionResult with the results
             yield FunctionResult(results)
