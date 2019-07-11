194751c9b169c4e0fd03231b06e75f3191d32b5f
Jared Fagel <jfagel@allete.com>
2019-07-11 13:21:31 -0500

    Initial Commit
    Initial Commit

---
+++ workflow processor scripts/Table Build | Pre-Processor | [Utility] CSV to JSON Structure.py
@@ -0,0 +1,6 @@
+inputs.incident_id = incident.id
+inputs.attachment_id = attachment.id
+inputs.attachment_name = attachment.name
+# inputs.csv_fields = 'Process Name, Last Access Time, Creation Time'  # Optional
+# inputs.row_limit = 200  # Optional
+# inputs.column_limit = 3  # Optional
