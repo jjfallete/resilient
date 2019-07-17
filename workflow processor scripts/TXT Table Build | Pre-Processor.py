8382b96006662ae0b454e2da271652e25a4f46c9
Jared Fagel <jfagel@allete.com>
2019-07-17 11:31:15 -0500

    Initial Commit.
    Initial Commit.

---
+++ workflow processor scripts/TXT Table Build | Pre-Processor.py
@@ -0,0 +1,5 @@
+inputs.incident_id = incident.id
+inputs.attachment_id = attachment.id
+inputs.attachment_name = attachment.name
+# inputs.split_rows_on_new_lines = False  # Optional, defaults to false
+# inputs.row_limit = 6  # Optional, defaults to no limit
