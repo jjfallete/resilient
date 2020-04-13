66a2070f04168c2711302a7fe93bdaf045d44509
Jared Fagel <jfagel@allete.com>
2020-04-13 14:25:39 -0500

    Removed make_query_string() method

--- functions/qradar/qradar/util/function_utils.py
+++ functions/qradar/qradar/util/function_utils.py
@@ -6,25 +6,6 @@
 import unicodedata
 
 
-def make_query_string(query, params):
-    """
-    Substitute parameters into the query
-    :param query: input query with params
-    :param params: values used to substitute
-    :return:
-    """
-    query_string = query
-
-    index = 1
-    for param in params:
-        if param:
-            to_replace = "%%param%d%%" % index
-            query_string = query_string.replace(to_replace, param)
-        index += 1
-
-    return query_string
-
-
 def fix_dict_value(events):
     """
     When the returned data from QRadar is used to update a datatable, we need to
